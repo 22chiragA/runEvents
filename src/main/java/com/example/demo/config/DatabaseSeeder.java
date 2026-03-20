086385d159c2a281c56b4e4e893ba79efc8fa41c
22chiragA <cagarstd05@gmail.com>
2026-03-20 16:19:01 +0530

    Auto seed super admin user admin@gmail.com

--- src/main/java/com/example/demo/config/DatabaseSeeder.java
+++ src/main/java/com/example/demo/config/DatabaseSeeder.java
@@ -1,17 +1,24 @@
 package com.example.demo.config;
 
 import com.example.demo.entity.Role;
+import com.example.demo.entity.User;
 import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseSeeder implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public DatabaseSeeder(RoleRepository roleRepository) {
+    public DatabaseSeeder(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -26,6 +33,18 @@ public class DatabaseSeeder implements CommandLineRunner {
             Role adminRole = new Role();
             adminRole.setName("ADMIN");
             roleRepository.save(adminRole);
+        }
+
+        if (userRepository.findByEmail("admin@gmail.com").isEmpty()) {
+            Role adminRole = roleRepository.findByName("ADMIN").orElseThrow(() -> new RuntimeException("Admin Role not found"));
+            User adminUser = User.builder()
+                    .name("Super Admin")
+                    .email("admin@gmail.com")
+                    .password(passwordEncoder.encode("admin123"))
+                    .role(adminRole)
+                    .isSubscribed(false)
+                    .build();
+            userRepository.save(adminUser);
         }
     }
 }
