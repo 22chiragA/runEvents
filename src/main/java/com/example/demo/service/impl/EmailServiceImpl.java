a40c5bd0621a6d02db80bb8424da41fcda37d602
22chiragA <cagarstd05@gmail.com>
2026-03-20 16:16:34 +0530

    Make email sending asynchronous to prevent Render SMTP timeout

--- src/main/java/com/example/demo/service/impl/EmailServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmailServiceImpl.java
@@ -8,9 +8,11 @@ import com.example.demo.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
+@Async
 public class EmailServiceImpl implements EmailService {
 
     @Autowired
